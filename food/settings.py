7c75fbc8058bb723af97a529829f579e92e59914
sudhanshu0022 <agarwalsudhanshu772@gmail.com>
2025-05-05 13:14:52 +0530

    error sove for deploy

--- food/settings.py
+++ food/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'eat.apps.EatConfig',  # Custom app for the food project
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
